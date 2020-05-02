c54c1831ea7ca89fa94f35eb7490881a9c574266
vagrant <vagrant@localhost.localdomain>
2020-05-02 06:25:47 +0000

    ログイン画面雛形

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ApplicationController
+class Admins::ProductsController < ApplicationController
   def top
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
- root 'users/products#top' 
+ root 'users/products#top'
  
   devise_for :admins, skip: :all
   devise_scope :admin do
-    get 'admin/sign_in' => 'admins/sessions#new', as: :new_admin_session
-    post 'admin/sign_in' => 'admins/sessions#create', as: :admin_session
-    delete 'admin/sign_out' => 'admins/sessions#destroy', as: :destroy_admin_session
+    get 'admins/sign_in' => 'admins/sessions#new', as: :new_admin_session
+    post 'admins/sign_in' => 'admins/sessions#create', as: :admin_session
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: :destroy_admin_session
   end
   
   devise_for :users, skip: :all
@@ -26,7 +26,9 @@ Rails.application.routes.draw do
   end
 
   namespace :admins do
-    resources :products, only: [:top, :index, :new, :show, :create, :edit, :update]
+    root 'products#top'
+    get 'products/top'
+    resources :products, only: [:index, :new, :show, :create, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :users, only: [:index, :show, :edit , :update]
     resources :orders, only: [:index, :show, :update]
