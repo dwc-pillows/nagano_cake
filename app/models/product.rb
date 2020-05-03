051c8dc25d8a61efa2ea238915137d850b46d44b
Sota <sooota09@gmail.com>
2020-05-03 18:51:40 +0900

    一時共有

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
-  attachment :image
+    belongs_to :genre
+    has_many :cart_items, dependent: :destroy
+    has_many :order_products, dependent: :destroy
+
+    attachment :image
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create(email: 'admin@admin.com', password: 'password')
+
+# genre
+genre_names = [
+  'ケーキ',
+  'キャンディ',
+  'プリン',
+  '焼き菓子'
+]
+
+genre_names.each do |genre_name|
+  Genre.create!(
+    name: genre_name,
+    is_active: true
+  )
+end
+# genre/
+
+4.times do |n|
+    Product.create!(
+        genre_id: 1,
+        name: "1",
+        description: "sample text",
+        price: 500,
+        is_active: true
+    )
+end
+
