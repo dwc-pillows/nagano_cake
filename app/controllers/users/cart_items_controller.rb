d7107d381dd29af4e709971464ef19b33a1e9e20
way-key <45gqz7@gmail.com>
2020-05-04 23:54:58 +0900

    [fix]カート追加機能

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -2,21 +2,51 @@ class Users::CartItemsController < ApplicationController
 
   def index
 
+    @cart_items = current_user.cart_items
+
   end
 
   def create
-
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.user_id = current_user.id
+    @cart_item.save
+    redirect_to users_cart_items_path
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    @user = User.find(current_user.id)
+    render "index"
 
   end
 
   def destroy
-
+    cart_item = CartItem.find(params[:id])
+    if cart_item.destroy
+      flash[:notice] = "カート内の商品を削除しました。"
+      redirect_to users_cart_items_path
+    else
+      @user = User.find(current_user.id)
+      render "index"
+    end
   end
 
   def destroy_all
-    
+    user = User.find(current_user.id)
+    if user.cart_items.destroy_all
+      flash[:notice] = "カート内の商品を全て削除しました。"
+      redirect_to users_cart_items_path
+    else
+      @user = User.find(current_user.id)
+      render "index"
+    end
   end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:user_id, :product_id, :amount)
+  end
+
 end
--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -1,15 +1,29 @@
 class Users::ProductsController < ApplicationController
 
   def top
-    
+
   end
-  
+
   def index
     @products = Product.all
   end
 
   def show
     @product = Product.find(params[:id])
+    @cart_item = CartItem.new
 
+    # ユーザーがログインしていない場合、itemsの内容を全商品とし、ログイン中はカートアイテムを対象にする
+    if current_user.nil?
+      items = CartItem.all
+    else
+      items = current_user.cart_items
+    end
+
+    # カートアイテム内の商品IDとitems内の商品IDに含まれていれば、商品詳細ページにエラーが出るようにする。
+    if items.pluck(:product_id).include?(@cart_item.product_id)
+      flash[:notice] = "商品はカートに追加済です"
+      redirect_back(fallback_location: root_path)
+    end
   end
+
 end
