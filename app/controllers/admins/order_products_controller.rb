2648a173e3610bf4d90bba6d81b8854b67fa75f4
way-key <45gqz7@gmail.com>
2020-05-09 17:04:42 +0900

    [update]管理者ページ > 注文機能の更新

--- app/controllers/admins/order_products_controller.rb
+++ app/controllers/admins/order_products_controller.rb
@@ -3,10 +3,18 @@ class Admins::OrderProductsController < ApplicationController
   before_action :user_block
 
   def update
-    
+    @order_products = OrderProduct.find(params[:id])
+    @order_products.update(order_products_params)
+    flash[:notice] = "制作ステータスが変更されました"
+    redirect_back(fallback_location: root_path)
   end
 
   private
+
+  def order_products_params
+    params.require(:order_product).permit(:production_status)
+  end
+
   def user_block
     if user_signed_in?
       redirect_to root_path
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,6 +2,7 @@ class OrderProduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+
   def taxed_product_price
     product.taxed_price
   end
