3dface06a8a04e35be84553fe745453e0c5f516e
way-key <45gqz7@gmail.com>
2020-05-09 11:20:48 +0900

    [fix]管理者の注文詳細画面を作成

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -18,7 +18,8 @@ class Admins::OrdersController < ApplicationController
   end
 
   def show
-
+    @order = Order.find(params[:id])
+    @order_products = @order.order_products
   end
 
   def update
