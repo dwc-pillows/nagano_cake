7b27c26423951fae005b5738d7f6b39765dad2d6
Keishi Yamamoto <keishi@KeishinoMacBook-Air.local>
2020-05-10 12:41:53 +0900

    [Update]enumの記述、integer型にのみ適用することとする。

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,15 @@ class Order < ApplicationRecord
   has_many :order_products, dependent: :destroy
   belongs_to :user
 
-  enum pay_method: {"クレジットカード": 0, "銀行振り込み": 1}
-
-
-  def total_price
-    total = 0
-    order_products.each do |order_product|
-      total += order_product.subtotal
-    end
-    total + postage
-  end
+  enum pay_method: {
+    "クレジットカード": 0,
+    "銀行振り込み": 1
+  }
+  enum order_status: {
+    "入金待ち": 0,
+    "入金確定": 1,
+    "制作中": 2,
+    "発送準備中": 3,
+    "発送済み": 4
+  }
 end
