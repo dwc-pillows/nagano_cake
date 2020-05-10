ba1a3d452d317a61574a4b5b7ed147ae848c668a
Keishi Yamamoto <keishi@KeishinoMacBook-Air.local>
2020-05-10 16:40:39 +0900

    [Fix]enumの記述を削除

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,13 +2,6 @@ class OrderProduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  enum production_status: {
-    "着手不可": 0,
-    "製作待ち": 1,
-    "制作中": 2,
-    "製作完了": 3
-  }
-
   def taxed_product_price
     product.taxed_price
   end
