4b706f8bf1e62b66e850898acdad15bc9aeae39b
Keishi Yamamoto <keishi@KeishinoMacBook-Air.local>
2020-05-10 16:33:45 +0900

    [Fix]orderモデルのenum修正、各viewの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,11 +6,4 @@ class Order < ApplicationRecord
     "クレジットカード": 0,
     "銀行振り込み": 1
   }
-  enum order_status: {
-    "入金待ち": 0,
-    "入金確定": 1,
-    "制作中": 2,
-    "発送準備中": 3,
-    "発送済み": 4
-  }
 end
