b6196f4dab874389d2627c198b5ccd4785605bf4
Keishi Yamamoto <keishi@KeishinoMacBook-Air.local>
2020-05-11 17:16:57 +0900

    [Fix]ステータス連動の修正

--- app/controllers/admins/order_products_controller.rb
+++ app/controllers/admins/order_products_controller.rb
@@ -3,9 +3,19 @@ class Admins::OrderProductsController < ApplicationController
   before_action :user_block
 
   def update
-    @order_products = OrderProduct.find(params[:id])
-    @order_products.update(order_products_params)
-    flash[:notice] = "製作ステータスが変更されました"
+    # 一つなので単数系に修正
+    @order_product = OrderProduct.find(params[:id])
+    @order_product.update(order_products_params)
+    @order = @order_product.order
+    # .any?でも代用可
+    if @order.order_products.where(production_status: 2).present?
+      @order.update(order_status: 2)
+    end
+    if @order.order_products.all? {
+      |order_product| order_product.production_status == 3}
+      @order.update(order_status: 3)
+    end
+    flash[:notice] = "ステータスが変更されました"
     redirect_back(fallback_location: root_path)
   end
 
